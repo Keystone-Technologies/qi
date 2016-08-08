3d7c6afadbf3c3111c88d1a5930d34d3b6e836a3
Ben Meyer <thebenmeyer@gmail.com>
2016-08-08 17:34:28 +0000

    added remove sub

--- lib/Qi/Controller/Assets.pm
+++ lib/Qi/Controller/Assets.pm
@@ -1,18 +1,12 @@
 package Qi::Controller::Assets;
 use Mojo::Base 'Mojolicious::Controller';
-
-# This action will render a template
-sub welcome {
-  my $self = shift;
-
-  # Render template "example/welcome.html.ep" with message
-  $self->render(msg => 'Welcome to the Mojolicious real-time web framework!');
-}
+use Qi::Model::Base;
+use Qi::Model::Assets;
 
 sub remove {
   my $self = shift;
   my $asset_tag = $self->param('asset_tag');
-  $self->stash('asset' => $self->assets->remove($asset_tag));
+  $self->stash('asset' => $self->assets->delete({asset_tag => $asset_tag}));
   $self->respond_to(
     json => {json => $self->stash('asset')},
   );
