fe497db238d2d2134930d403818776ef024fbb39
Ben Meyer <thebenmeyer@gmail.com>
2016-08-11 21:01:02 +0000

    created Assets Controller

--- lib/Qi/Controller/Assets.pm
+++ lib/Qi/Controller/Assets.pm
@@ -1,12 +1,41 @@
 package Qi::Controller::Assets;
 use Mojo::Base 'Mojolicious::Controller';
 use Qi::Model::Base;
-use Qi::Model::Assets;
+use SQL::Abstract;
+
+my $table = 'assets';
+
+sub get {
+  my $self = shift;
+  my $response = $self->req->json;
+  $self->stash('asset' => $self->base->select($response , $table));
+  $self->respond_to(
+    json => {json => $self->stash('asset')},
+  );
+}
+
+sub insert {
+  my $self = shift;
+  my $response = $self->req->json;
+  $self->stash('asset' => $self->base->insert($response, $table));
+  $self->respond_to(
+    json => {json => $self->stash('asset')},
+  );
+}
+
+sub update {
+  my $self = shift;
+  my $response = $self->req->json;
+  $self->stash('asset' => $self->base->update($response , $table));
+  $self->respond_to(
+    json => {json => $self->stash('asset')},
+  );
+}
 
 sub remove {
   my $self = shift;
-  my $asset_tag = $self->param('asset_tag');
-  $self->stash('asset' => $self->assets->delete({asset_tag => $asset_tag}));
+  my $response = $self->req->json;
+  $self->stash('asset' => $self->base->delete($response , $table));
   $self->respond_to(
     json => {json => $self->stash('asset')},
   );
