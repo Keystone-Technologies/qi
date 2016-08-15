c2dc87eb08510b08cb188fabf0ff3fa04de182fb
Ben Meyer <thebenmeyer@gmail.com>
2016-08-15 17:35:07 +0000

    no longer stash json response

--- lib/Qi/Controller/Assets.pm
+++ lib/Qi/Controller/Assets.pm
@@ -9,36 +9,28 @@ sub get {
   my $self = shift;
   my $response = $self->req->json;
   $self->stash('asset' => $self->base->select($response , $table));
-  $self->respond_to(
-    json => {json => $self->stash('asset')},
-  );
+  $self->render(json => $self->base->select($response, $table));
 }
 
 sub insert {
   my $self = shift;
   my $response = $self->req->json;
   $self->stash('asset' => $self->base->insert($response, $table));
-  $self->respond_to(
-    json => {json => $self->stash('asset')},
-  );
+  $self->render(json => $self->base->select($response, $table));
 }
 
 sub update {
   my $self = shift;
   my $response = $self->req->json;
   $self->stash('asset' => $self->base->update($response , $table));
-  $self->respond_to(
-    json => {json => $self->stash('asset')},
-  );
+  $self->render(json => $self->base->select($response, $table));
 }
 
 sub remove {
   my $self = shift;
   my $response = $self->req->json;
   $self->stash('asset' => $self->base->delete($response , $table));
-  $self->respond_to(
-    json => {json => $self->stash('asset')},
-  );
+  $self->render(json => $self->base->select($response, $table));
 }
 
 1;
