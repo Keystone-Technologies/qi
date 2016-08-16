3fe24c6f4b548b252cb5c1195bc826938b7dfa2b
Ben Meyer <thebenmeyer@gmail.com>
2016-08-16 20:14:39 +0000

    Assets Controller now uses Assets Model

--- lib/Qi/Controller/Assets.pm
+++ lib/Qi/Controller/Assets.pm
@@ -1,36 +1,49 @@
 package Qi::Controller::Assets;
 use Mojo::Base 'Mojolicious::Controller';
-use Qi::Model::Base;
-use SQL::Abstract;
-
-my $table = 'assets';
+use Qi::Model::Assets;
 
 sub get {
   my $self = shift;
-  my $response = $self->req->json;
-  $self->stash('asset' => $self->base->select($response , $table));
-  $self->render(json => $self->base->select($response, $table));
+  my $json = $self->req->json;
+  $self->render(
+    json => {
+      table => 'assets',
+      (($json) ? 'asset' : 'assets') => $self->assets->select($json)
+    }
+  );
 }
 
 sub insert {
   my $self = shift;
-  my $response = $self->req->json;
-  $self->stash('asset' => $self->base->insert($response, $table));
-  $self->render(json => $self->base->select($response, $table));
+  warn $self->req->json;
+  $self->render(
+    json => {
+      table => 'assets',
+      tag => $self->assets->insert($self->req->json)
+    }
+  );
 }
 
 sub update {
   my $self = shift;
-  my $response = $self->req->json;
-  $self->stash('asset' => $self->base->update($response , $table));
-  $self->render(json => $self->base->select($response, $table));
+  my $json = $self->req->json;
+  $self->render(
+    json => {
+      table => 'assets',
+      status => $self->assets->update($json)
+    }
+  );
 }
 
 sub remove {
   my $self = shift;
-  my $response = $self->req->json;
-  $self->stash('asset' => $self->base->delete($response , $table));
-  $self->render(json => $self->base->select($response, $table));
+  my $json = $self->req->json;
+  $self->render(
+    json => {
+      table => 'assets',
+      status => $self->assets->delete($json)
+    }
+  );
 }
 
 1;
