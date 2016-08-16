878909dd17cf9c81d0a65d3dc02ecd660b978139
Ben Meyer <thebenmeyer@gmail.com>
2016-08-16 20:15:22 +0000

    created Assets Model to subclass base

--- lib/Qi/Model/Assets.pm
+++ lib/Qi/Model/Assets.pm
@@ -1,2 +1,18 @@
-package Qi::Controller::Assets;
-use Mojo::Base;
+package Qi::Model::Assets;
+use Mojo::Base 'Qi::Model::Base';
+
+has table => 'assets';
+
+#sub update_timestamp {
+#    my ($self, $tag) = @_;
+#    $self->sql
+#    my $results = eval {
+#        #my $sql = 'update ? set add_stamp= (now) where tag=?';
+#        #$self->pg->db->query($sql, $self->table, $tag)->hash;
+#        $self->update({data => {add_stamp => \'now()'}, where => {tag => $tag}})
+#        
+#    }
+#    if ($@){ warn "there's an error";}
+#    else {warn "Success!";}
+#}
+1;
